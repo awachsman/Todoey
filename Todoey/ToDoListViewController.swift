e8a987bd36d92923570407a4bf2b166787809bde
awachsman <sysmaven@gmail.com>
2020-04-26 17:00:12 -0400

    Create itemArray to hold data; write tableView functions to get array counbt and to populate prototype cell with array data

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
+    
+    //Create an array with 3 entries; these are todo items which will populate cells 1, 2 and 3 in the tableView
+    let itemArray = ["Buy eggs", "Study Swift", "Go out to Dinner"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
 }
-
