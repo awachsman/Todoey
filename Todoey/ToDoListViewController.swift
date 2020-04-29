15fe6393b4911d4d5964c1d4a9b86443c3974f23
awachsman <sysmaven@gmail.com>
2020-04-29 10:20:42 -0400

    Add bar button item to navigation bar in Storyboard; add IBAction to present UIAlertController with an integrated text field;  create and process added item and refresh tableView

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    //Create an array with 3 entries; these are todo items which will populate cells 1, 2 and 3 in the tableView
-    let itemArray = ["Buy eggs", "Study Swift", "Go out to Dinner"]
+    //Create an array with 3 todo items which will populate cells 1, 2 and 3 in the tableView
+    var itemArray = ["Buy eggs", "Study Swift", "Go out to Dinner"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -26,7 +24,6 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")!
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         cell.textLabel?.text = itemArray[indexPath.row]
@@ -36,13 +33,13 @@ class ToDoListViewController: UITableViewController {
     }
     
     //MARK: - TableView Delegate Methods
-    // This function initially printed the selected array item when the item was clicked. Once this was demonstrated to work, print was commented out and instead, a checkmark accessory is invoked for the selected item Once selected, the row flashes gray briefly then reverts to nonselected look
+    // This function initially printed the selected array item when the item was clicked. Once this was demonstrated to work, print was commented out and instead, a checkmark accessory is invoked for the selected item. The selected row flashes gray briefly then reverts to nonselected look
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // Print the selected array item (for test purposes)
         //print(itemArray[indexPath.row])
         
-        // Check to see if seleted cell already has a checkmark accessory.  If it does, remove the checkmark.  If it doesn't add the checkmark accessory
+        // If seleted cell already has a checkmark accessory, remove it, otherwiseadd it
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -53,4 +50,39 @@ class ToDoListViewController: UITableViewController {
         // Finally, revert to non-selected look for the cell
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //NOTE:  Understanding of scope for variables is necessary to follow sequence of events below
+        //1. Initialize an empty text field called textField.  It is accessible throughout this IBAction and will hold whatever is placed into the text field we're creating in the UIAlertController
+        var textField = UITextField()
+        
+        //2. Create the UIAlertContoller popup with a placeholder
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        //3. Create the action
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //the code in this closure determines  what happens when the Add Item button in the UIAlert is pressed
+            self.itemArray.append(textField.text!) //texfield.text was created in 4b. below
+            self.tableView.reloadData() // refresh the tableView to display added item
+            
+        }
+        
+        //4. Add the text field to the alert: name the text field alertTextField
+        alert.addTextField { (alertTextField) in
+            // 4a. set up the text field's attributes
+            alertTextField.placeholder = "Create new item"
+            //4b. textfield can't be printed from here (since at this point we're just presenting the UIAlertController) but we can store whatever value the user types into the textField var which is accessible throughut the IBAction.
+            textField = alertTextField
+        }
+        //5. Add the action which was created in steps 3 and 4 to the alert
+        alert.addAction(action)
+        
+        //6. Present the alert
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
